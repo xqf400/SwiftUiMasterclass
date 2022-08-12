e81a5f9e458c6ca4ea001adb8946b8eeab9a7761
Fabian Kuschk <8header8@googlemail.com>
2022-08-12 10:16:01 +0200

    Async image an drestart app start

---
+++ Apps/Restart/Restart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Fabian Kuschke on 12.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Apps/Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Fabian Kuschke on 12.08.22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
