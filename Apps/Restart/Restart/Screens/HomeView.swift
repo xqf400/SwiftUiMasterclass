199fc3b08c4500344b903c1b7e8adc21faad1318
Fabian Kuschk <8header8@googlemail.com>
2022-08-15 16:09:14 +0200

    home screen

--- Apps/Restart/Restart/Screens/HomeView.swift
+++ Apps/Restart/Restart/Screens/HomeView.swift
@@ -15,14 +15,42 @@ struct HomeView: View {
     //MARK: - Body
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
+//            Text("Home")
+//                .font(.largeTitle)
+            
+            //MARK: Header
+            
+            Spacer()
+            Image("character-2")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
+            //MARK: Center
+            
+            Text("The time taht leads to mastery is dependent to the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            //MARK: Footer
+            
+            Spacer()
             
             Button {
                 isOnoardingViewActive = true
             } label: {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
                 Text("Restart")
-            }
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }//Button
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
 
         }//Vtsack
     }
