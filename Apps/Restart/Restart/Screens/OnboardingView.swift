74638ec75a6e7d5d11a6ceec461fee413a3218ea
Fabian Kuschk <8header8@googlemail.com>
2022-08-15 12:08:29 +0200

    onboarding screen

--- Apps/Restart/Restart/Screens/OnboardingView.swift
+++ Apps/Restart/Restart/Screens/OnboardingView.swift
@@ -73,10 +73,60 @@ struct OnboardingView: View {
                             .frame(width: 260, height: 260, alignment: .center)
                     }
                     Image("character-1")
-                      .resizable()
-                      .scaledToFit()
+                        .resizable()
+                        .scaledToFit()
                 }//: CENTER
                 
+                //MARK: - Footer
+                
+                ZStack{
+                    
+                    //1. Background
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    //2. Call to action
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    //3. Capsule
+                    
+                    HStack{
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    //4. Circle (draggable)
+                    
+                    HStack {
+                        ZStack{
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnoardingViewActive = false
+                        }
+                        Spacer()
+                    }//HStack
+                }//Footer
+                .frame(height: 80, alignment: .center)
+                .padding()//avoid screen edges
             } //: VSTACK
         } //: ZSTACK
         
