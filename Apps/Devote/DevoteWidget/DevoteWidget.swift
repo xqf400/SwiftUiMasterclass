876ec338671cb616cf07407aee2bd76f8559b962
Fabian Kuschk <8header8@googlemail.com>
2022-09-06 10:45:51 +0200

    end of widget

--- Apps/Devote/DevoteWidget/DevoteWidget.swift
+++ Apps/Devote/DevoteWidget/DevoteWidget.swift
@@ -40,9 +40,59 @@ struct SimpleEntry: TimelineEntry {
 
 struct DevoteWidgetEntryView : View {
     var entry: Provider.Entry
+    
+    @Environment(\.widgetFamily) var widgetFamily
+    
+    var fontStyle : Font {
+        if widgetFamily == .systemSmall {
+            return .system(.footnote, design: .rounded)
+        }else{
+            return .system(.headline, design: .rounded)
+        }
+    }
 
     var body: some View {
-        Text(entry.date, style: .time)
+        //Text(entry.date, style: .time)
+        GeometryReader { geometry in
+            ZStack{
+                backgroundGradient
+                Image("rocket-small")
+                    .resizable()
+                    .scaledToFit()
+                
+                Image("logo")
+                    .resizable()
+                    .frame(
+                        width: widgetFamily != .systemSmall ? 56 : 36,
+                        height: widgetFamily != .systemSmall ? 56 : 36
+                    )
+                    .offset(
+                        x: (geometry.size.width / 2) - 20,
+                        y: (geometry.size.height / -2) + 20
+                    )
+                    .padding(.top, widgetFamily != .systemSmall ? 32 : 12)
+                    .padding(.trailing, widgetFamily != .systemSmall ? 32 : 12)
+                
+                HStack {
+                    Text("Just do it")
+                        .foregroundColor(.white)
+                        .font(fontStyle)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 4)
+                        .background(
+                            Color(red: 0, green: 0, blue: 0, opacity: 0.5)
+                                .blendMode(.overlay)
+                        )
+                    .clipShape(Capsule())
+                    if widgetFamily != .systemSmall {
+                        Spacer()
+                    }
+                }//HStack
+                .padding()
+                .offset(y: (geometry.size.height / 2) - 24)
+            }//Zstack
+        }//geometry
     }
 }
 
@@ -54,8 +104,8 @@ struct DevoteWidget: Widget {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             DevoteWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Devote Launcher")
+        .description("This is an example widget for the personal task manager.")
     }
 }
 
