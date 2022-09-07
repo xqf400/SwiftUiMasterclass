b72aecdf3dfbbd91b9973f1b4dd8a5610c84b2bd
Fabian Kuschk <8header8@googlemail.com>
2022-09-07 13:55:42 +0200

    added images etc

---
+++ Apps/Honeymoon/Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Fabian Kuschke on 07.09.22.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
