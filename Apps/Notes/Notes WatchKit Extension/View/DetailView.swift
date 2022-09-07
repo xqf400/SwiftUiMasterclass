8bf49bd120386a32bc9c7893880c1cc9e4126d65
Fabian Kuschk <8header8@googlemail.com>
2022-09-07 12:35:13 +0200

    creditsview

--- Apps/Notes/Notes WatchKit Extension/View/DetailView.swift
+++ Apps/Notes/Notes WatchKit Extension/View/DetailView.swift
@@ -16,14 +16,7 @@ struct DetailView: View {
     var body: some View {
         VStack(alignment: .center, spacing: 3){
             //Header
-            HStack{
-                Capsule()
-                    .frame(height: 1)
-                Image(systemName: "note.text")
-                
-                Capsule()
-                    .frame(height: 1)
-            }
+            HeaderView(title: "")
             //Content
             Spacer()
             ScrollView(.vertical){
