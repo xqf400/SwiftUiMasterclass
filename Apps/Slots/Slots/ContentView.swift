5556c53112f5c8390052671a12920dca04855913
Fabian Kuschk <8header8@googlemail.com>
2022-09-14 16:42:51 +0200

    Slots init

---
+++ Apps/Slots/Slots/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Slots
+//
+//  Created by Fabian Kuschke on 14.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Apps/Slots/Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Fabian Kuschke on 14.09.22.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
