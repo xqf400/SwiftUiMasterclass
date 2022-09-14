35d1ec4725083d432c746e92363b15b945c7d58a
Fabian Kuschk <8header8@googlemail.com>
2022-09-14 14:37:07 +0200

    load 2 cards

--- Apps/Honeymoon/Honeymoon/View/CardView.swift
+++ Apps/Honeymoon/Honeymoon/View/CardView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct CardView: View {
+struct CardView: View, Identifiable {
     
     //MARK: Properties
     
@@ -58,7 +58,7 @@ struct CardView: View {
 struct CardView_Previews: PreviewProvider {
     
     static var previews: some View {
-        CardView(honeymonn: honeymoonData[1])
+        CardView(honeymonn: honeymoonData[0])
             .previewLayout(.fixed(width: 375, height: 600))
     }
 }
--- Apps/Honeymoon/Honeymoon/View/ContentView.swift
+++ Apps/Honeymoon/Honeymoon/View/ContentView.swift
@@ -13,14 +13,43 @@ struct ContentView: View {
     @State var showAlert: Bool = false
     @State var showGuide: Bool = false
     @State var showInfo: Bool = false
+    
+    //MARK: Card views
+    
+    var cardViews: [CardView] = {
+        var views = [CardView]()
+        for index in 0..<2 {
+            views.append(CardView(honeymonn: honeymoonData[index]))
+        }
+        return views
+    }()
+    
+    //MARK: Top card
+    private func isTopCard(cardView:CardView) -> Bool{
+        guard let index = cardViews.firstIndex(where: {$0.id == cardView.id}) else{
+            return false
+        }
+        return index == 0
+    }
 
     var body: some View {
         VStack {
+            //MARK: Header
             HeaderView(showGuideView: $showGuide, showInfoView: $showInfo)
             Spacer()
-            CardView(honeymonn: honeymoonData[2])
-                .padding()
+            
+            //MARK: Cards
+            ZStack{
+                ForEach(cardViews){ cardView in
+                    cardView
+                        .zIndex(self.isTopCard(cardView: cardView) ? 1 : 0)
+                }
+            }
+            //CardView(honeymonn: honeymoonData[2])
+               // .padding()
             Spacer()
+            
+            //MARK: Footer
             FooterView(showBookingAlert: $showAlert)
         }
         .alert(isPresented: $showAlert) {
